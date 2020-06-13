5cf6c907182b04f02f7d5f08dff21f01589930bc
Artem Shabanov <artemziz@yandex.ru>
2020-06-13 12:32:28 +0300

    error processing

--- popup.js
+++ popup.js
@@ -8,26 +8,31 @@ let Up = document.getElementById('Up');
 
 
 moveBtn.addEventListener('click',()=>{
-    let x1 = document.getElementById('x1').value;
-    let y1 = document.getElementById('y1').value;
-    let x2 = document.getElementById('x2').value;
-    let y2 = document.getElementById('y2').value;
-    let speed = document.getElementById('speed').value || 2500;
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let x1 = +document.getElementById('x1').value;
+    let y1 = +document.getElementById('y1').value;
+    let x2 = +document.getElementById('x2').value;
+    let y2 = +document.getElementById('y2').value;
+    let speed = +document.getElementById('speed').value || 2500;
+    if(x1 && y1 && x2 && y2 && speed){
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"moveTo": {x1:x1,y1:y1,x2:x2,y2:y2,speed:speed}});
       });
+    }
+    
 })
 
 clickBtn.addEventListener('click',()=>{
-  let x = document.getElementById('x').value;
-  let y = document.getElementById('y').value;
+  let x = +document.getElementById('x').value;
+  let y = +document.getElementById('y').value;
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"click": {x:x,y:y}});
-  });
+  if(x && y){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"click": {x:x,y:y}});
+    });
+  }
+  
   
 })
 
